bb64f1eea0a550be43beef556a6d8285c81ce5d7
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-05-14 14:21:13 +0200

    refractor(vehicle.cpp): change the coordinates of the vehicle so that we have the size and position of each end of the vehicle

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -4,33 +4,37 @@ using namespace std;
 
 class Vehicle {
     public:
-        int x;
-        int y;
+        int x_top;
+        int y_top;
+        int x_bottom;
+        int y_bottom;
 
-        Vehicle(int x, int y){
-            this->x = x;
-            this->y = y;
+        Vehicle(int x_top, int x_botton ,int y_top, int y_bottom){
+            this->x_top = x_top;
+            this->x_bottom = x_bottom;
+            this->y_top = y_top;
+            this->y_bottom = y_bottom;
         }
 
         void moveUP(){
-            y++;
+            y_top++;
+            y_bottom++;
         }
 
         void moveDOWN(){
-            y--;
+            y_top--;
+            y_bottom--;
         }
 
         void moveLEFT(){
-            x--;
+            x_top--;
+            x_bottom--;
         }
 
         void moveRIGHT(){
-            x++;
+            x_top++;
+            x_bottom++;
         }
-
-        void print(){
-            cout << "Vehicle is at: " << x << ", " << y << endl;
-        }
-
+        
         ~Vehicle(){};
 };
