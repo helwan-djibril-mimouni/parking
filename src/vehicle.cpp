20562aa6d8b3f92f40173d4feabd02e605b24e50
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-05-14 14:30:21 +0200

    refractor(vehicle.cpp):  change variable name y_top to y_left and y_botton to y_right

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -5,25 +5,25 @@ using namespace std;
 class Vehicle {
     public:
         int x_top;
-        int y_top;
+        int y_left;
         int x_bottom;
-        int y_bottom;
+        int y_right;
 
-        Vehicle(int x_top, int x_botton ,int y_top, int y_bottom){
+        Vehicle(int x_top, int x_botton ,int y_left, int y_right){
             this->x_top = x_top;
             this->x_bottom = x_bottom;
-            this->y_top = y_top;
-            this->y_bottom = y_bottom;
+            this->y_left = y_left;
+            this->y_right = y_right;
         }
 
         void moveUP(){
-            y_top++;
-            y_bottom++;
+            y_left++;
+            y_right++;
         }
 
         void moveDOWN(){
-            y_top--;
-            y_bottom--;
+            y_left--;
+            y_right--;
         }
 
         void moveLEFT(){
@@ -35,6 +35,6 @@ class Vehicle {
             x_top++;
             x_bottom++;
         }
-        
+
         ~Vehicle(){};
 };
