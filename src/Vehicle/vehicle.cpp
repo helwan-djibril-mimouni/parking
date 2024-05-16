d0dd85d2d34716a8e8819d15ac92bde3ab5bde20
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-05-16 08:13:54 +0200

    refractor(vehicle.cpp): the vehicle class inherits from the class GameObject

--- src/Vehicle/vehicle.cpp
+++ src/Vehicle/vehicle.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
+#include "GameObject.hpp"
 
 using namespace std;
 
-class Vehicle {
+class Vehicle : public GameObject{
     public:
         int x_top;
         int y_left;
         int x_bottom;
         int y_right;
 
-        Vehicle(int x_top, int x_botton ,int y_left, int y_right){
-            this->x_top = x_top;
-            this->x_bottom = x_bottom;
-            this->y_left = y_left;
-            this->y_right = y_right;
-        }
+        Vehicle(int x_top, int x_botton ,int y_left, int y_right) : GameObject(x_top, x_bottom, y_left, y_right){}
 
         void moveUP(){
             y_left++;
